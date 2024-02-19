a7d167d26f2586de1f7fdd1fa3b035c630e8860d
GODGUYHW <85698300+GODGUYHW@users.noreply.github.com>
2024-02-19 15:05:26 +0700

    FIX Move and color
    If in that place has the same color as current figure then it can not move to that place. if it is not the same color the current figure will go get that piece.

--- Projects(js)/Project 1/LabOop/Lab3/BoardGame/board.java
+++ Projects(js)/Project 1/LabOop/Lab3/BoardGame/board.java
@@ -3,7 +3,7 @@ package LabOop.Lab3.BoardGame;
 public class board {
     int position1, position2;
     int rows = 8, columns = 8;
-    char[][] board = new char[rows][columns];
+    String[][] board = new String[rows][columns];
     figure player1, player2;
     char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
 
@@ -22,77 +22,96 @@ public class board {
         } else {
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < columns; j++) {
-                    if (player1.getRow() == i && player1.getColumns() == j) {
-                        board[i][j] = player1.getName().charAt(0);
-                    } else if (player2.getRow() == i && player2.getColumns() == j) {
-                        board[i][j] = player2.getName().charAt(0);
+                    if (player1.getRow() - 1 == i && player1.getColumns() - 1 == j) {
+                        board[i][j] = player1.getColor() ? "W" + player1.getName().substring(0, 1)
+                                : "B" + player1.getName().substring(0, 1);
+                    } else if (player2.getRow() - 1 == i && player2.getColumns() - 1 == j) {
+                        board[i][j] = player2.getColor() ? "W" + player2.getName().substring(0, 1)
+                                : "B" + player1.getName().substring(0, 1);
                     } else {
-                        board[i][j] = '-';
+                        board[i][j] = "xx";
                     }
                 }
             }
             System.out.print("   ");
             for (int i = 0; i < columns; i++) {
-                System.out.print(" ");
+                System.out.print("    ");
                 System.out.print(letters[i]);
             }
             System.out.println();
-            System.out.println("----------------------------");
+            System.out.println("--------------------------------------------------------");
             for (int i = 0; i < rows; i++) {
                 System.out.print(i + 1);
                 System.out.print(" |");
                 for (int j = 0; j < columns; j++) {
-                    System.out.print(" ");
+                    System.out.print("   ");
                     System.out.print(board[i][j]);
                 }
-                System.out.print("|");
+                System.out.print(" |");
                 System.out.println("");
             }
-            System.out.println("----------------------------");
+            System.out.println("--------------------------------------------------------");
         }
     }
 
     public void freeMove(figure player, String locations) {
         int moveRow = 0;
         int moveColumns = 0;
+        int isWhite = 0;
+        int isBlack = 0;
+
+        // turn string into int first character
         char toInt = locations.charAt(0);
         if (toInt >= 'a' && toInt <= 'h') {
             moveColumns = moveColumns * 16 + (toInt - 'a' + 1);
         } else {
-            System.out.println("Invalid character: " + toInt);
+            System.out.println("Invalid character: " + toInt + "!!!");
             return;
         }
 
+        // turn second character into int
         toInt = locations.charAt(1);
         if (toInt >= '0' && toInt <= '8') {
             moveRow = moveRow * 16 + (toInt - '0');
         } else {
-            System.out.println("Invalid character: " + toInt);
+            System.out.println("Invalid character: " + toInt + "!!!");
             return;
         }
-        board[player.getRow()][player.getColumns()] = '-';
-        board[moveRow - 1][moveColumns - 1] = player.getName().charAt(0);
-        boardDisplay();
+        if (player.getColor() == true && board[moveRow - 1][moveColumns - 1].charAt(0) == 'W') {
+            System.out.println("Can not move to that place");
+            return;
+        } else if (player.getColor() == false && board[moveRow - 1][moveColumns - 1].charAt(0) == 'B') {
+            System.out.println("Can not move to that place");
+            return;
+        } else if (player.getColor() == true) {
+            board[player.getRow() - 1][player.getColumns() - 1] = "xx";
+            board[moveRow - 1][moveColumns - 1] = "W" + player.getName().substring(0, 1);
+            boardDisplay(player, moveRow - 1, moveColumns - 1);
+        } else if (player.getColor() == false) {
+            board[player.getRow() - 1][player.getColumns() - 1] = "xx";
+            board[moveRow - 1][moveColumns - 1] = "B" + player.getName().substring(0, 1);
+            boardDisplay(player, moveRow - 1, moveColumns - 1);
+        }
     }
 
-    public void boardDisplay() {
+    public void boardDisplay(figure player, int moveRow, int moveColumns) {
         System.out.print("   ");
         for (int i = 0; i < columns; i++) {
-            System.out.print(" ");
+            System.out.print("    ");
             System.out.print(letters[i]);
         }
         System.out.println();
-        System.out.println("----------------------------");
+        System.out.println("--------------------------------------------------------");
         for (int i = 0; i < rows; i++) {
             System.out.print(i + 1);
             System.out.print(" |");
             for (int j = 0; j < columns; j++) {
-                System.out.print(" ");
+                System.out.print("   ");
                 System.out.print(board[i][j]);
             }
-            System.out.print("|");
+            System.out.print(" |");
             System.out.println("");
         }
-        System.out.println("----------------------------");
+        System.out.println("--------------------------------------------------------");
     }
 }
--- Projects(js)/Project 1/LabOop/Lab3/BoardGame/main.java
+++ Projects(js)/Project 1/LabOop/Lab3/BoardGame/main.java
@@ -3,12 +3,12 @@ package LabOop.Lab3.BoardGame;
 public class main {
     // Black = 0 , White = 1
     public static void main(String[] args) {
-        figure[] figures = new figure[2];
-        figures[0] = new figure("GUY", 0, 2, 4);
-        figures[1] = new figure("BUY", 1, 4, 1);
+        figure player1 = new figure("Queen", 0, 2, 4);
+        figure player2 = new figure("Pawn", 1, 4, 1);
+        figure player3 = new figure("King", 1, 2, 8);
         board b = new board();
-        b.setBoard(figures[0], figures[1]);
-        b.freeMove(figures[0], "b8");
-        b.freeMove(figures[0], "h1");
+        b.setBoard(player1, player2);
+        b.freeMove(player1, "a2");
+        b.freeMove(player2, "a2");
     }
 }

b3123914128925fc2f8a4073369b3930b074de71
GODGUYHW <85698300+GODGUYHW@users.noreply.github.com>
2024-02-19 23:05:16 +0700

    FIX color to String

--- Projects(js)/Project 1/LabOop/Lab3/BoardGame/figure.java
+++ Projects(js)/Project 1/LabOop/Lab3/BoardGame/figure.java
@@ -4,14 +4,22 @@ public class figure {
     String name;
     int row, columns;
     int BLACK = 0;
-    int WHITE = 1;
-    int color;
+    int WHITE = 0;
+    String color;
 
-    public figure(String name, int color, int row, int columns) {
+    public figure(String name, String color, int row, int columns) {
         this.name = name;
         this.row = row;
         this.columns = columns;
         this.color = color;
+        if (color.toLowerCase() == "white") {
+            WHITE = 1;
+        } else if (color.toLowerCase() == "black") {
+            BLACK = 1;
+        } else if (color.toLowerCase() != "white" || color.toLowerCase() != "black") {
+            System.out.println("Enter only white or black");
+            System.exit(0);
+        }
     }
 
     public String getName() {
@@ -27,7 +35,7 @@ public class figure {
     }
 
     public boolean getColor() {
-        if (color == WHITE) {
+        if (WHITE == 1) {
             return true;
         } else {
             return false;
--- Projects(js)/Project 1/LabOop/Lab3/BoardGame/main.java
+++ Projects(js)/Project 1/LabOop/Lab3/BoardGame/main.java
@@ -1,12 +1,12 @@
 package LabOop.Lab3.BoardGame;
 
 public class main {
-    // Black = 0 , White = 1
     public static void main(String[] args) {
-        figure player1 = new figure("Queen", 0, 2, 4);
-        figure player2 = new figure("Pawn", 1, 2, 8);
-        figure player3 = new figure("King", 1, 2, 8);
+        figure player1 = new figure("Queen", "white", 2, 4);
+        figure player2 = new figure("Pawn", "white", 4, 1);
+        figure player3 = new figure("King", "black", 2, 8);
         board b = new board();
+        b.boardDisplay();
         b.addFigure(player1);
         b.addFigure(player2);
         b.addFigure(player3);
